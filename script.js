2622e86cd8843efbdc3e7dd8fcfabcbdd071364a
DiannaNguyen <diannavnguyen@gmail.com>
2021-02-24 21:50:17 -0600

    Working logic on displaying question and answers - and indication on correct answer

--- script.js
+++ script.js
@@ -1,105 +1,8 @@
-// declare a funtion that would start the game when start button clicked
-// declare a function that would allow player to click on and answer choice
-// allow player to click on the continue button to continue to next question - ONLY if answered correctly
+// allow player to click on the continue button to continue to next question - ONLY if answered correctly??
 // display "correct"/"incorrect" and corresponding colors on button when answered right or wrong
 // randomly questions pop up and answer choices are mixed
 
-const questionBox = document.querySelector('.question-box');
-const catergoryBox = document.querySelector(".category");
-const questionsText = document.querySelector('.questions-text');
-const correctText = document.querySelector(".correct-text");
-const incorrectText = document.querySelector(".incorrect-text");
-const optionsBox = document.querySelector('.answers-box');
-const continueButton = document.querySelector(".continue-next-question");
-
-
-// const questionBox = document.querySelector("#question-box");
-// const categoryBox = document.querySelector(".category");
-// const questionsText = document.querySelector(".questions-text");
-// const optionsBox = document.querySelector(".answers-box");
-// const correctButton = document.querySelector(".correct");
-// const incorrectButton = document.querySelector(".incorrect");
-// const correctText = document.querySelector("correct");
-// const incorrectText = document.querySelector("incorrect");
-// const continueButton = document.querySelector(".continue");
-
-let currentQuestion;
-let remainingQuestions = [];
-let remainingOptions = [];
-
-
-// push questions into empty array - remainingQuestions
-function setRemainingQuestions() {
-    for (let i = 0; i < questions.length; i++) {
-        remainingQuestions.push(questions[i]);
-    }
-};
-
-function nextQuestion() {
-    // randomize questions
-    const randomQuestions = remainingQuestions[Math.floor(Math.random() * remainingQuestions.length)];
-    currentQuestion = randomQuestions;
-    // display category
-    categoryBox.innerText = currentQuestion.category;
-	console.log(randomQuestions);
-    // display questionsText
-    questionsText.innerText = currentQuestion.question;
-    
-    // find index of the randomQuestions in the remainingQuestion array - remove that question from the array so it doesn't repeat (splice)
-    // Reference: https://www.w3schools.com/js/js_array_iteration.asp & https://www.w3schools.com/jsref/jsref_splice.asp
-    
-    const arrayQuestions = remainingQuestions.indexOf(randomQuestions);
-    remainingQuestions.splice(arrayQuestions, 1);
-    // push options into empty array - remainingOptions
-    for (let i = 0; i < currentQuestion.options.length; i++) {
-        remainingOptions.push(i);
-    }
-}
-
-
-// set options (answer choices)
-function displayAnswers() {
-        //randomize answer choices
-        const randomOptions =
-            remainingOptions[Math.floor(Math.random() * remainingOptions.length)];
-    
-        // find index of the randomOptions in the remainingOptions array - remove that options from the array so it doesn't repeat when the correlating question is called (splice)
-        const arrayO = remainingOptions.indexOf(randomOptions);
-            remainingOptions.splice(arrayO, 1);
-        
-        const option = document.createElement("btn");
-        option.innerText = currentQuestion.options[randomOptions];
-        option.classList.add("button");
-
-        optionsBox.appendChild(option);
-}
-
-
-
-
-
-        
-
-
-//  function selectAnswer() {
-//     if(selectAnswer === question[i].answer) {
-//        nextQuestion(); 
-//     } else {
-//         return "GAME OVER";
-//     }
-
-// }
-
-
-
-
-
-
-
-
-
-
-//Reference: Questions and correct answers: https://thoughtcatalog.com/samantha-newman/2020/03/250-best-trivia-questions/#general-trivia-questions & https://thoughtcatalog.com/january-nelson/2021/02/sports-trivia-questions/ & https://thoughtcatalog.com/katee-fletcher/2020/04/history-trivia-questions/ 
+//Reference: Questions and correct answers: https://thoughtcatalog.com/samantha-newman/2020/03/250-best-trivia-questions/#general-trivia-questions & https://thoughtcatalog.com/january-nelson/2021/02/sports-trivia-questions/ & https://thoughtcatalog.com/katee-fletcher/2020/04/history-trivia-questions/
 const questions = [
 	{
 		category: 'Geography',
@@ -121,7 +24,7 @@ const questions = [
 	{
 		category: 'Geography',
 		question: 'What is the smallest ocean in the world?',
-		options: ['The Pacific', 'The Indian', 'The Antarctic', 'The Artic'],
+		options: ['The Pacific', 'The Indian', 'The Antarctic', 'The Arctic'],
 		answer: 3,
 	},
 	{
@@ -133,14 +36,14 @@ const questions = [
 	{
 		category: 'Sports',
 		question:
-			'When Michael Jordan played for the Chicago Bulls, how many NBA Championships did he win?',
+		'When Michael Jordan played for the Chicago Bulls, how many NBA Championships did he win?',
 		option: ['6', '3', '0', '8'],
 		answer: 0,
 	},
 	{
 		category: 'Sports',
 		question:
-			'What is the only team in the NFL to neither host nor play in the Super Bowl?',
+		'What is the only team in the NFL to neither host nor play in the Super Bowl?',
 		options: [
 			'Baltimore Ravens',
 			'Detroit Lions',
@@ -152,7 +55,7 @@ const questions = [
 	{
 		category: 'Sports',
 		question:
-			'Which is the only team to play in every soccer World Cup tournment?',
+		'Which is the only team to play in every soccer World Cup tournment?',
 		options: ['Brazil', 'Argentina', 'France', 'Italy'],
 		answer: 0,
 	},
@@ -194,7 +97,7 @@ const questions = [
 	{
 		category: 'Pop Culture',
 		question:
-			'Which 2008 comedy had the tagline “Put this in your pipe and smoke it”?',
+		'Which 2008 comedy had the tagline “Put this in your pipe and smoke it”?',
 		options: [
 			'Mama Mia',
 			'Pineapple Express',
@@ -222,3 +125,90 @@ const questions = [
 		answer: 1,
 	},
 ];
+
+const categoryBox = document.querySelector('.category');
+const questionsText = document.querySelector('.questions-text');
+const correctText = document.querySelector('.correct-text');
+const incorrectText = document.querySelector('.incorrect-text');
+const optionsBox = document.querySelector('.answers-box');
+const continueButton = document.querySelector('.continue-next-question');
+const correctColor = document.querySelector(
+	'.question-box .answers-box .option.correct'
+);
+
+let currentQuestion;
+let remainQuestions = [];
+let remainOptions = [];
+
+continueButton.addEventListener('click', nextQuestion);
+
+// push questions into empty array - remainQuestions
+function setRemainQuestions() {
+	const totalQuestion = questions.length;
+	for (let i = 0; i < totalQuestion; i++){
+		remainQuestions.push(questions[i]);
+	}
+}
+
+function nextQuestion() {
+	// randomize questions
+	const randomQuestions = remainQuestions[Math.floor(Math.random() * remainQuestions.length)];
+	currentQuestion = randomQuestions;
+	
+	// display category
+	categoryBox.innerText = currentQuestion.category;
+	
+	// display questionsText
+	questionsText.innerText = currentQuestion.question;
+	
+	// find index of the randomQuestions in the remainQuestion array - remove that question from the array so it doesn't repeat (splice)
+	// Reference: https://www.w3schools.com/js/js_array_iteration.asp & https://www.w3schools.com/jsref/jsref_splice.asp
+	const arrayQuestions = remainQuestions.indexOf(randomQuestions);
+	remainQuestions.splice(arrayQuestions, 1);
+	
+	// set options - answer choices
+	// ge the length of options
+	const totalOptions = currentQuestion.options.length;
+	for (let i = 0; i < totalOptions; i++) {
+		remainOptions.push(i);
+	}
+
+	optionsBox.innerText = "";
+
+	 for (let i = 0; i < totalOptions; i++) {
+			// const randomOptions =
+			// 	remainOptions[Math.floor(Math.random() * remainOptions.length)];
+			// const arrayOptions = remainOptions.indexOf(randomOptions);
+			// remainOptions.splice(arrayOptions, 1);
+
+			const option = document.createElement('div');
+			option.innerText = currentQuestion.options[i];
+			option.id = i;
+			option.className = 'option';
+			option.addEventListener("click", () => {
+				console.log(currentQuestion);
+				console.log(option);
+				if(currentQuestion.answer == option.id){
+					option.style.backgroundColor = "limegreen";
+					option.style.color = "black";
+				} else {
+
+				}
+			}) 
+			optionsBox.appendChild(option);
+			
+		}
+}
+
+// function results(){
+// 	const optionChoice = currentQuestion.answer;
+// 	if(optionChoice === optionChoice) {
+// 		correctText.classList.remove("hide");
+// 	} else {
+// 		incorrectText.classList.remove("hide");
+// 	}
+// }
+
+
+setRemainQuestions();
+nextQuestion();
